b251206738a9b1de89507f543c415db5f3dea942
Shedrach Adegene <shedrach@oeaconsults.com>
2024-06-30 21:06:50 +0100

    “”

--- AWSGithubActions/spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ AWSGithubActions/spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController 
 {
 @RequestMapping("/")
-public String hello() 
+public String hellew() 
 {
 return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
 }
